8162554449d6d12418b5a729dd748b24bfe4887f
cyberop99poc-cmd <cyberop.99poc@gmail.com>
2026-02-19 17:08:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ── Middleware ────────────────────────────────────────────────
-app.use(cors());
+app.use(cors({
+  origin: ["https://frontend2-dainzs-projects.vercel.app/"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
